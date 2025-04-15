ec2be953c14e0e96ff288788dc78ef6aec123669
Erik Oliveira <erikmoraesoliveira900@gmail.com>
2025-04-14 22:20:11 -0300

    Commit inicial

---
+++ src/main/java/br/com/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.screenmatch;
+
+import br.com.screenmatch.model.DadosSerie;
+import br.com.screenmatch.services.ConsumoApi;
+import br.com.screenmatch.services.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=Peaky+Blinders&apikey=73b6571c");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
---
+++ src/main/java/br/com/screenmatch/services/IConverteDados.java
@@ -0,0 +1,6 @@
+package br.com.screenmatch.services;
+
+public interface IConverteDados {
+
+    <T> T obterDados (String json, Class<T> classe);
+}
